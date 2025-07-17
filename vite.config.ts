ed812d2bdd849d9d552a1cc1f73304560141d668
Michael Bozhilov <michaelbozhilov@gmail.com>
2025-07-17 17:12:29 +0300

    Generate types for package

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts';
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: 'simple-result',
+      fileName: 'simple-result',
+    },
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'nested/index.html'),
-      },
+      external: [],
+      output: {},
     },
   },
+  plugins: [dts({
+    entryRoot: 'lib',
+    outDir: 'dist/types',
+  })],
 })
